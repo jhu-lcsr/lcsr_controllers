3ddcb31b1e836d2a8b2a1c8d98fa94ca0bb3420a
Jonathan Bohren <jonathan.bohren@gmail.com>
2014-01-15 04:03:06 -0500

    Adding missing controllers to plugin library

--- src/lcsr_controllers.cpp
+++ src/lcsr_controllers.cpp
@@ -3,8 +3,12 @@
 
 #include "id_controller_kdl.h"
 #include "joint_pid_controller.h"
+#include "joint_traj_generator_kdl.h"
+#include "semi_absolute_calibration_controller.h"
 
 ORO_CREATE_COMPONENT_LIBRARY()
 ORO_LIST_COMPONENT_TYPE(lcsr_controllers::IDControllerKDL)
 ORO_LIST_COMPONENT_TYPE(lcsr_controllers::JointPIDController)
+ORO_LIST_COMPONENT_TYPE(lcsr_controllers::JointTrajGeneratorKDL)
+ORO_LIST_COMPONENT_TYPE(lcsr_controllers::SemiAbsoluteCalibrationController)
 
